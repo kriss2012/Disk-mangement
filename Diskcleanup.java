55524e81aa71858ce076b0bbfce4d076c0217f08
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-06-23 23:58:45 +0530

    Update Diskcleanup.java

--- Diskcleanup.java
+++ Diskcleanup.java
@@ -1,1 +1,35 @@
+import java.io.File;
 
+public class DiskCleanup {
+
+    public static void deleteTempFiles(String path) {
+
+        File folder = new File(path);
+
+        if (!folder.exists()) {
+            System.out.println("Path not found!");
+            return;
+        }
+
+        File[] files = folder.listFiles();
+
+        if (files == null) {
+            return;
+        }
+
+        int count = 0;
+
+        for (File file : files) {
+
+            if (file.getName().endsWith(".tmp")) {
+
+                if (file.delete()) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(
+                count + " temporary files deleted.");
+    }
+}
