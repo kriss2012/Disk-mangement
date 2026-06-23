c9ee3c14a745620dfc865e0bff20e5ad5f4ea7d4
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-06-23 23:56:23 +0530

    Update Diskmanager.java

--- Diskmanager.java
+++ Diskmanager.java
@@ -1,1 +1,27 @@
+import java.io.File;
 
+public class DiskManager {
+
+    public void displayDiskInformation() {
+
+        File[] drives = File.listRoots();
+
+        for (File drive : drives) {
+
+            long total = drive.getTotalSpace();
+            long free = drive.getFreeSpace();
+            long used = total - free;
+
+            System.out.println("Drive: " + drive);
+            System.out.println("Total: " + convert(total));
+            System.out.println("Used : " + convert(used));
+            System.out.println("Free : " + convert(free));
+            System.out.println("-----------------------");
+        }
+    }
+
+    private String convert(long bytes) {
+        return String.format("%.2f GB",
+                bytes / (1024.0 * 1024 * 1024));
+    }
+}
