d20a61aa1eba961d409477184af6bf7a09abfb90
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-06-23 23:57:55 +0530

    Update Filesanner.java

--- Filesanner.java
+++ Filesanner.java
@@ -1,1 +1,30 @@
+import java.io.File;
 
+public class FileScanner {
+
+    public static void scanFolder(String path) {
+
+        File folder = new File(path);
+
+        if (!folder.exists()) {
+            System.out.println("Folder not found!");
+            return;
+        }
+
+        File[] files = folder.listFiles();
+
+        if (files == null) {
+            return;
+        }
+
+        for (File file : files) {
+
+            if (file.isFile()) {
+                System.out.println(
+                        "File : " + file.getName() +
+                        " | Size : " +
+                        file.length() + " bytes");
+            }
+        }
+    }
+}
